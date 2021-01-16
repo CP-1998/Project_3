32523e8249b5ed9f48233d9475ba41fbc8d5fcc2
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2021-01-16 12:22:31 -0800

    redesigned both sign-up and sign-in pages, added X buttons to both forms (non-functional)

--- client/src/pages/signin/signin.js
+++ client/src/pages/signin/signin.js
@@ -5,11 +5,13 @@ import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link'
 
 
+
 function App() {
   return (
     <body>
       <Box id="signInCard" color='primary'>
-        <p>Sign In</p>
+        <img alt="logo placeholder" id="logoSignIn" src={'https://www.goomlandscapes.co.nz/wp-content/uploads/2018/08/logo-placeholder.png'}></img>
+        <Button id="xBtn" variant="outlined" color="primary">X</Button>
         <hr></hr>
         <TextField label="Enter your email" />
         <br></br>
--- client/src/pages/signup/signup.js
+++ client/src/pages/signup/signup.js
@@ -1,19 +1,36 @@
+import './signup.css'
 import React from 'react'
 import { Box, TextField, Button, Link } from '@material-ui/core'
 
 const SignUp = () => {
   return (
     <body>
-      <Box id="signInCard" color='primary'>
-        <p>Sign Up</p>
+      <Box id="signUpCard" color='primary'>
+        <img alt="logo placeholder" id="logoSignIn" src={'https://www.goomlandscapes.co.nz/wp-content/uploads/2018/08/logo-placeholder.png'}></img>
+        <Button id="xBtn" variant="outlined" color="primary">X</Button>
         <hr></hr>
+        <TextField type="name" id="firstName" label="First Name" />
+        <br></br>
+        <br></br>
+        <br></br>
+        <TextField type="name" id="firstName" label="First Name" />
+        <br></br>
+        <br></br>
+        <br></br>
         <TextField type="email" id="emailInput" label="Enter your email" />
         <br></br>
         <br></br>
-        <TextField id="passwordHash" label="Password" type="password" autoComplete="current-password" />
+        <br></br>
+        <TextField id="passwordHash" label="Enter Password" type="password" autoComplete="current-password" />
+        <br></br>
+        <br></br>
+        <br></br>
+        <TextField id="passwordHash" label="Verify Password" type="password" autoComplete="current-password" />
+        <br></br>
         <br></br>
         <br></br>
         <Link href="signin">Already have an account?</Link>
+        <br></br>
         <br></br>
         <br></br>
         <Button variant='contained' color='primary'>
