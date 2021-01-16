8e98883be308aeebe3b3a6b951277bc27bb073de
Nicole Uva <nicouva@gmail.com>
2021-01-16 13:42:05 -0500

    user authentication routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,19 @@
 const router = require('express').Router()
 const { User } = require('../models')
-// const passport = require('passport')
-// const jwt = require('jsonwebtoken')
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
 
-router.post('/users/register', (req, res) => {
-  const { firstName, lastName, username, email, password } = req.body
-  User.register(new User({ firstName, lastName, username, password, email }), password, err => {
+router.post('/users/signup', (req, res) => {
+  const { firstName, lastName, email, password } = req.body
+  User.signup(new User({ firstName, lastName, email, password }), password, err => {
     if (err) { console.log(err) }
     res.sendStatus(200)
   })
 })
 
-router.post('/users/login', (req, res) => {
-  const { username, password } = req.body
-  User.authenticate()(username, password, (err, user) => {
+router.post('/users/signin', (req, res) => {
+  const { email, password } = req.body
+  User.authenticate()(email, password, (err, user) => {
     if (err) { console.log(err) }
     res.json(user ? jwt.sign({ id: user._id }, process.env.SECRET) : null)
   })
